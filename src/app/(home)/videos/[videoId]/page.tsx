1317941b9301cc1e0d7bfe6a8b8ccd6cbd387591
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2025-01-31 02:45:10 +0100

    18: video page

---
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -0,0 +1,23 @@
+import { HydrateClient, trpc } from "@/trpc/server";
+
+import { VideoView } from "@/modules/videos/ui/views/video-view";
+
+interface PageProps {
+  params: Promise<{
+    videoId: string;
+  }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { videoId } = await params;
+
+  void trpc.videos.getOne.prefetch({ id: videoId });
+
+  return ( 
+    <HydrateClient>
+      <VideoView videoId={videoId} />
+    </HydrateClient>
+  );
+};
+ 
+export default Page;
